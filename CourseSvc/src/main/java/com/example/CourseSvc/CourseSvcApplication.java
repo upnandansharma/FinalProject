6dd0c81db7fbc3c8cd6c29dab2c54ca16670a8c8
upnandansharma <139189231+upnandansharma@users.noreply.github.com>
2024-02-11 20:55:49 +0530

    Initial commit

---
+++ CourseSvc/src/main/java/com/example/CourseSvc/CourseSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseSvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSvcApplication.class, args);
+	}
+
+}
---
+++ CourseSvc/src/test/java/com/example/CourseSvc/CourseSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseSvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
